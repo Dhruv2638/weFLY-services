6ba970ee388446535f09a741d4108bb4211c912b
DhruvKumar Nagar <iamdhruvnagar@gmail.com>
2023-06-27 08:56:10 +0530

    Added API for Delete City

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     InfoController : require('./info-controller.js'),
     AirplaneController: require('./airplane-controller.js'),
-    CityConroller: require('./city-controller.js')
+    CityController: require('./city-controller.js')
 }
