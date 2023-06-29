b58dd96e5068d0b08e6c901057345439b63edf56
DhruvKumar Nagar <iamdhruvnagar@gmail.com>
2023-06-29 10:44:59 +0530

    Added some more filters related to sorting

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -26,6 +26,8 @@ async function createFlight(data) {
 
 async function getAllFlights(query) {
   let customFilter = {};
+  let sortFilter = [];
+  const endingTripTime = " 23:59:00";
   if (query.trips) {
     [departureAirportId, arrivalAirportId] = query.trips.split("-");
     customFilter.departureAirportId = departureAirportId;
@@ -38,8 +40,30 @@ async function getAllFlights(query) {
       [Op.between]: [minPrice, maxPrice == undefined ? 20000 : maxPrice],
     };
   }
+
+  if (query.travellers) {
+    customFilter.totalSeats = {
+      [Op.gte]: query.travellers,
+    };
+  }
+
+  if (query.tripDate) {
+    customFilter.departureTime = {
+      [Op.between]: [query.tripDate, query.tripDate + endingTripTime],
+    };
+  }
+
+  if (query.sort) {
+    const params = query.sort.split(",");
+    const sortFilters = params.map((param) => param.split("_"));
+
+    sortFilter = sortFilters;
+  }
   try {
-    const flights = await flightRepository.getAllFlights(customFilter);
+    const flights = await flightRepository.getAllFlights(
+      customFilter,
+      sortFilter
+    );
     return flights;
   } catch (error) {
     throw new AppError(
